09e6ced49bf3e1f95fcd20cf56b33c3d5290d5c3
Keshav Malik <keshav@Keshavs-MacBook-Air.local>
2023-06-16 02:33:38 +0530

    fourth

--- src/index.js
+++ src/index.js
@@ -1,9 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import Header from './components/Header';
 import './index.css';
-import Hero from './pages/Hero';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
